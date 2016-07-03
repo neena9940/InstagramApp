02aed055ead7f20012d8751ac2b2343a94bd0ee2
neena9940 <ninayousefi@ninas-macbook-pro.local>
2016-07-03 12:54:13 +0430

    refreshButton pressed

--- Photos/ViewController.m
+++ Photos/ViewController.m
@@ -22,9 +22,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.logoutButton.enabled = false;
+    self.refreshButton.enabled = false;
+
 }
 - (IBAction)loginButtonPressed:(id)sender {
     [[NXOAuth2AccountStore sharedStore]requestAccessToAccountWithType:@"Instagram"];
+    self.loginButton.enabled = false;
+    self.logoutButton.enabled = true;
+    self.refreshButton.enabled = true;
+
 
 }
 - (IBAction)logoutButtonPressed:(id)sender {
@@ -34,9 +41,75 @@
         [share removeAccount:acct];
         
     }
+    self.loginButton.enabled = true;
+    self.logoutButton.enabled = false;
+    self.refreshButton.enabled = false;
+
 
 }
 - (IBAction)refreshButtonPressed:(id)sender {
+    NSArray *instagramAccounts = [[NXOAuth2AccountStore sharedStore] accountsWithAccountType:@"Instagram"];
+    if ([instagramAccounts count] == 0){
+        NSLog(@"Warning %ld instagram accounts logged in.", (long)[instagramAccounts count]);
+        return;
+    }
+    NXOAuth2Account *acct = instagramAccounts[0];
+    NSString *token = acct.accessToken.accessToken;
+    //Profile
+    //NSString *urlString = [@"https://api.instagram.com/v1/users/self/?access_token=" stringByAppendingString:token];
+    //Recent Liked Media
+    NSString *urlString = [@"https://api.instagram.com/v1/users/self/?access_token=" stringByAppendingString:token];
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    
+    //    NSURLRequest *request = [[NSURLRequest alloc]initWithURL:url];
+    //    NSURLSessionDownloadTask *task = [session downloadTaskWithRequest:request completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+    //        NSString *text = [[NSString alloc]initWithContentsOfURL:location encoding:NSUTF8StringEncoding error:nil];
+    //        NSLog(@"text: %@", text);
+    //    }];
+    //    [task resume];
+    [[session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        
+        //Check for network error
+        if(error){
+            NSLog(@"Error! Couldn't finish request! %@", error);
+            return;
+        }
+        
+        //Check for Http error
+        NSHTTPURLResponse *httpResp = (NSHTTPURLResponse *)response;
+        if(httpResp.statusCode <200 || httpResp.statusCode >=300){
+            NSLog(@"Error! Get status code %ld ", (long)httpResp.statusCode);
+            return;
+        }
+        //Check for Json parse error
+        NSError *parseError;
+        id pkg = [NSJSONSerialization JSONObjectWithData:data options:0 error:&parseError];
+        if(!pkg){
+            NSLog(@"Error! Couldn't parse response! %@", parseError);
+            return;
+        }
+        // NSString *imgURLStr = pkg[@"data"][@"images"][@"standard_resolution"][@"url"];
+        NSString *imgURLStr = pkg[@"data"][@"profile_picture"];
+        NSString *usernameStr = pkg[@"data"][@"username"];
+        NSString *fullnameStr = pkg[@"data"][@"full_name"];
+        NSURL *imageURL = [NSURL URLWithString:imgURLStr];
+        // NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
+        
+        // self.imageView.image = [UIImage imageWithData:imageData];
+        [[session dataTaskWithURL:imageURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            //
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.imageView.image = [UIImage imageWithData:data];
+               // self.usernameLabel.text = [NSString stringWithFormat:@"Welcome, %@!", usernameStr];
+              //  self.fullnameLabel.text = fullnameStr;
+            });
+        }]resume];
+        
+    }]resume];
+
 }
 
 - (void)didReceiveMemoryWarning {
