0ef0282d398c735a7c7ac22e472dbc1b1bc4c6bd
neena9940 <ninayousefi@ninas-macbook-pro.local>
2016-07-03 12:34:31 +0430

    Initial Commit

---
+++ Photos/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Photos
+//
+//  Created by Nina Yousefi on 7/3/16.
+//  Copyright © 2016 Nina Yousefi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
